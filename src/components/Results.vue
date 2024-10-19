9abe0cf3eb10e644cb794b2f57504c5c51462256
Stefan Etarsky <stefan.etarsky@gmail.com>
2024-10-18 20:44:17 -0400

    finish project

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,32 @@
 <template>
     <h3>Reaction time: {{ score }} ms</h3>
+    <h5 class="rank">{{ rank }}</h5>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid reflexes'
+        } else {
+            this.rank = 'Snail pace'
+        }
+    }
 }
 </script>
 
 <style>
-
+ .rank {
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+ }
 </style>
